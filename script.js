3ef9dd8ed6f33f16fd92ca6d89a5671a5861718e
Cameron Chandler <webstormcam@gmail.com>
2023-11-14 21:20:47 -0500

    Was able to connect to the database

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
+const appSettings = {
+    databaseURL: "https://we-the-champs-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const congratsInDB = ref(database, "congrats")
+
+
+
+
+
+
+
+
+
+
 const textPlace = document.getElementById('textPlace')
 const to0 = document.getElementById('to')
 const from0 = document.getElementById('from')
-
-
-
-
-
-
-
-
 const button = document.getElementById('submitButton')
 
 button.addEventListener('click',function(event){
@@ -19,6 +29,13 @@ button.addEventListener('click',function(event){
         alert("Please fill out all fields!!")
         event.preventDefault()
     } else{
+        const values ={
+            "value":textPlace.value,
+            "to":to0.value,
+            "from":from0.value
+        }
+        push(congratsInDB,values)
+        console.log(values)
         textPlace.value=""
         to0.value=""
         from0.value=""
