163d77ec9e7fda72c3ebf65138ab214c6e12610a
Cameron Chandler <webstormcam@gmail.com>
2023-11-18 00:54:54 -0500

    Completed the project!
    Made a Thanks app. Where users can post their thanks for other users. You can click on them and delete if you no longer want that thanks to be seen. The data base that holds all the Congrats that users submit is using Firebase!

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
+
 const appSettings = {
     databaseURL: "https://we-the-champs-default-rtdb.europe-west1.firebasedatabase.app/"
 }
@@ -20,9 +21,10 @@ const congratsInDB = ref(database, "congrats")
 
 
 const textPlace = document.getElementById('textPlace')
-const to0 = document.getElementById('to')
-const from0 = document.getElementById('from')
+// const to0 = document.getElementById('to')
+// const from0 = document.getElementById('from')
 const button = document.getElementById('submitButton')
+const thanksArea= document.getElementById('endorsement-area')
 
 button.addEventListener('click',function(event){
     if (textPlace.value==""){
@@ -39,11 +41,42 @@ button.addEventListener('click',function(event){
 onValue(congratsInDB,function(snapshot){
     if(snapshot.exists()){
         let itemsArray =Object.entries(snapshot.val())
-        console.log(itemsArray)
+        clearList()
+       itemsArray.reverse()
+        for(let i=0;i<itemsArray.length;i++){
+            const congrat =itemsArray[i][1]
+            const del_code = itemsArray[i][0]
+            createSquare(congrat,del_code)
+        }
+    } else{
+        thanksArea.innerHTML="No thanks here... yet"
     }
 
-    // for (let i=0;i<itemsArray.length;i++){
-    //     let currentItem= itemsArray[i]
-    // }
+    
 })
+
+
+function clearList(){
+    thanksArea.innerHTML=""
+}
+
+function createSquare(value,number){
+    const el = document.createElement('div')
+    el.classList.add('endorsement')
+    const p = document.createElement('p')
+    p.classList.add('endorse-words')
+    p.textContent=value
+    el.append(p)
+    
+    
+    // `<div class="endorsement">
+    // <p class="endorse-words">${value}</p>
+    // </div>`
+    el.addEventListener("click",function(){
+        let exactLocationOfItemInDB = ref(database, `congrats/${number}`)
+        remove(exactLocationOfItemInDB)
+    })
+    thanksArea.append(el)
+
+}
 
